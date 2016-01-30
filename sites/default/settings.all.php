63e7135a7d0469c38a533f88cc734053af16f989
Michael Schmid <michael@amazeelabs.com>
2016-01-30 19:30:34 +0100

    more overrides

--- sites/default/settings.all.php
+++ sites/default/settings.all.php
@@ -11,3 +11,4 @@ $settings['hash_salt'] = 'iunoiuspoifuapoisufnpoi';
 $config_directories['sync'] = 'sites/default/config/sync';
 
 $config['system.site']['name'] = 'overwritten sitename';
+$config['system.site']['page']['403'] = 'asdf';
